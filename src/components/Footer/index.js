94354a47e9f2e7d468bd344acb579288f3f90abf
rikicop <ruperto1@protonmail.com>
2021-06-25 12:27:13 -0400

    social m commit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,49 +1,78 @@
 import React from 'react'
+import {FooterContainer, FooterWrap, FooterLinksContainer, 
+        FooterLinksWrapper, FooterLinkItems, FooterLinkTitle, 
+        FooterLink 
+    } from './FooterElements';
 
 const Footer = () => {
     return (
-        <FootContainer>
+        <FooterContainer>
             <FooterWrap>
                 <FooterLinksContainer>
                     <FooterLinksWrapper>
                         <FooterLinkItems>
-                            <FooterLinkTitle> About us</FooterLinkTitle>   
-                                <FooterLink to="/signin">How it Works</FooterLink>
-                                <FooterLink to="/signin">Testimonials</FooterLink>
-                                <FooterLink to="/signin">Careers</FooterLink>
-                                <FooterLink to="/signin">Investors</FooterLink>
-                                <FooterLink to="/signin">Terms of Service</FooterLink>  
+                            <FooterLinkTitle> About us</FooterLinkTitle>
+                            <FooterLink to="/signin">How it Works</FooterLink>
+                            <FooterLink to="/signin">Testimonials</FooterLink>
+                            <FooterLink to="/signin">Careers</FooterLink>
+                            <FooterLink to="/signin">Investors</FooterLink>
+                            <FooterLink to="/signin">Terms of Service</FooterLink>
                         </FooterLinkItems>
                         <FooterLinkItems>
-                            <FooterLinkTitle> About us</FooterLinkTitle>   
-                                <FooterLink to="/signin">How it Works</FooterLink>
-                                <FooterLink to="/signin">Testimonials</FooterLink>
-                                <FooterLink to="/signin">Careers</FooterLink>
-                                <FooterLink to="/signin">Investors</FooterLink>
-                                <FooterLink to="/signin">Terms of Service</FooterLink>  
+                            <FooterLinkTitle> About us</FooterLinkTitle>
+                            <FooterLink to="/signin">How it Works</FooterLink>
+                            <FooterLink to="/signin">Testimonials</FooterLink>
+                            <FooterLink to="/signin">Careers</FooterLink>
+                            <FooterLink to="/signin">Investors</FooterLink>
+                            <FooterLink to="/signin">Terms of Service</FooterLink>
                         </FooterLinkItems>
                     </FooterLinksWrapper>
                     <FooterLinksWrapper>
                         <FooterLinkItems>
-                            <FooterLinkTitle> About us</FooterLinkTitle>   
-                                <FooterLink to="/signin">How it Works</FooterLink>
-                                <FooterLink to="/signin">Testimonials</FooterLink>
-                                <FooterLink to="/signin">Careers</FooterLink>
-                                <FooterLink to="/signin">Investors</FooterLink>
-                                <FooterLink to="/signin">Terms of Service</FooterLink>  
+                            <FooterLinkTitle> About us</FooterLinkTitle>
+                            <FooterLink to="/signin">How it Works</FooterLink>
+                            <FooterLink to="/signin">Testimonials</FooterLink>
+                            <FooterLink to="/signin">Careers</FooterLink>
+                            <FooterLink to="/signin">Investors</FooterLink>
+                            <FooterLink to="/signin">Terms of Service</FooterLink>
                         </FooterLinkItems>
                         <FooterLinkItems>
-                            <FooterLinkTitle> About us</FooterLinkTitle>   
-                                <FooterLink to="/signin">How it Works</FooterLink>
-                                <FooterLink to="/signin">Testimonials</FooterLink>
-                                <FooterLink to="/signin">Careers</FooterLink>
-                                <FooterLink to="/signin">Investors</FooterLink>
-                                <FooterLink to="/signin">Terms of Service</FooterLink>  
+                            <FooterLinkTitle> About us</FooterLinkTitle>
+                            <FooterLink to="/signin">How it Works</FooterLink>
+                            <FooterLink to="/signin">Testimonials</FooterLink>
+                            <FooterLink to="/signin">Careers</FooterLink>
+                            <FooterLink to="/signin">Investors</FooterLink>
+                            <FooterLink to="/signin">Terms of Service</FooterLink>
                         </FooterLinkItems>
                     </FooterLinksWrapper>
                 </FooterLinksContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to='/'>
+                            Dolla
+                        </SocialLogo>
+                        <WebsiteRights>Dolla © {new Date().getFullYear()}
+                        Todos Los Derechos Reservados</WebsiteRights>
+                        <SocialIcons>
+                            <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                                <FaFacebook />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                                <FaFacebook />
+                            </SocialIconLink><SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                                <FaFacebook />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                                <FaFacebook />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                                <FaFacebook />
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
             </FooterWrap>
-        </FootContainer>       
+        </FooterContainer>
     )
 }
 
